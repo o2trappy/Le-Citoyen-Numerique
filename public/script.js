5d07571e7c4ecee5b0a012a5e4e19586a3810aa6
o2trappy <otengland@gmail.com>
2025-01-23 11:04:43 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 // Données d'exemple pour les épisodes
 const episodes = [
-  { titre: "Épisode 1: Introduction", fichier: "episodes/episode1.m4a" },
-  { titre: "Épisode 2: Les bases", fichier: "episodes/episode2.m4a" },
-  { titre: "Épisode 3: Techniques avancées", fichier: "episodes/episode3.m4a" },
+  { titre: "Épisode 1: Introduction", fichier: "episodes/episode1.mp3" },
+  { titre: "Épisode 2: Les bases", fichier: "episodes/episode2.mp3" },
+  { titre: "Épisode 3: Techniques avancées", fichier: "episodes/episode3.mp3" },
 ]
 
 // Fonction pour afficher les épisodes
